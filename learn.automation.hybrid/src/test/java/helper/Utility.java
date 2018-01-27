72b6e9b4f0daf0e5b7113a746ec33e242a583142
Mukesh_50 <Mukesh_50@192.168.0.102>
2018-01-27 09:40:21 +0530

    Signed-off-by: Mukesh_50 <Mukesh_50@192.168.0.102>

--- learn.automation.hybrid/src/test/java/helper/Utility.java
+++ learn.automation.hybrid/src/test/java/helper/Utility.java
@@ -7,9 +7,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Utility {
 
@@ -29,22 +33,62 @@ public class Utility {
 
 		String screenshotPath = System.getProperty("user.dir") + "/Screenshots/" + Utility.getCurrentDateTime()
 				+ ".png";
-		
-		File dest=new File(screenshotPath);
-		
-		TakesScreenshot ts=(TakesScreenshot)driver;
-		
-		File src=ts.getScreenshotAs(OutputType.FILE);
-		
-		try 
-		{
+
+		File dest = new File(screenshotPath);
+
+		TakesScreenshot ts = (TakesScreenshot) driver;
+
+		File src = ts.getScreenshotAs(OutputType.FILE);
+
+		try {
 			FileUtils.copyFile(src, dest);
 		} catch (IOException e) {
 			System.out.println("ERROR: Unable to capture Screenshot");
-			System.out.println("ERROR: "+e.getMessage());
+			System.out.println("ERROR: " + e.getMessage());
 		}
 
 		return screenshotPath;
 	}
+
+	public static void highLightElement(WebDriver driver, WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
+
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+
+			System.out.println(e.getMessage());
+		}
+
+		js.executeScript("arguments[0].setAttribute('style','border: solid 2px white');", element);
+
+	}
+	
+	
+	public static void syncWebElement(WebDriver driver,WebElement element,int time)
+	{
+		
+		WebDriverWait wait=new WebDriverWait(driver,time);
+		wait.until(ExpectedConditions.visibilityOf(element));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+		
+	}
+	
+	
+	public static WebElement waitForWebElement(WebDriver driver,WebElement element,int time)
+	{
+		
+		syncWebElement(driver, element, time);
+		
+		highLightElement(driver, element);
+		
+		return element;
+	}
+	
+	
+	
+	
 
 }
